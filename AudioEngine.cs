5e6a8fb1d32e20e20be2cc50943c8ae4f5a08426
GreezXII <GreezXII@protonmail.ch>
2021-07-19 16:55:18 +0300

    Implement pattern with accented and normal beats

--- AudioEngine.cs
+++ AudioEngine.cs
@@ -9,16 +9,17 @@ namespace Metronome
         private readonly MixingSampleProvider mixer;
 
         private string accentedBeatPath = "Sounds/snare.wav";
-        //private string normalBeatPath = "Sounds/hi-hat.wav";
+        private string normalBeatPath = "Sounds/hi-hat.wav";
 
         public SampleSource accentedBeat;
-        //private SampleSource normalBeat;
+        private SampleSource normalBeat;
 
         public AudioEngine(int sampleRate = 44100, int channelCount = 2)
         {
             accentedBeat = new SampleSource(accentedBeatPath);
-            Pattern patternEngine = new Pattern();
-            SampleSource pattern = patternEngine.CreatePattern(accentedBeat);
+            normalBeat = new SampleSource(normalBeatPath);
+            PatternEngine patternEngine = new PatternEngine();
+            SampleSource pattern = patternEngine.CreatePattern(accentedBeat, normalBeat);
 
             outputDevice = new WaveOut();
             mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channelCount));
---
+++ PatternEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Metronome
+{
+    class PatternEngine
+    {
+        public int BPM { get; set; }
+        public int Measure { get; set; }
+        private double BeatDuration { get; set; }
+
+        public PatternEngine(int bpm = 200, int measure = 4)
+        {
+            BPM = bpm;
+            BeatDuration = 60.0 / BPM / 4;
+            Measure = measure;
+        }
+
+        public SampleSource CreatePattern(SampleSource accentedBeat, SampleSource normalBeat)
+        {
+            long step = (long)(accentedBeat.WaveFormat.SampleRate * accentedBeat.WaveFormat.Channels * (accentedBeat.WaveFormat.BitsPerSample / 8) * BeatDuration);
+            long length = step * Measure;
+
+            float[] buffer = new float[length];
+
+            long index = 0;
+            SampleSource beat;
+            while (index < buffer.Length)
+            {
+                //  Copy accented beat first
+                if (index == 0)
+                    beat = accentedBeat;
+                else
+                    beat = normalBeat;
+
+                if (beat.Length > step)
+                    Array.Copy(beat.AudioData, 0, buffer, index, step);
+                else
+                    Array.Copy(beat.AudioData, 0, buffer, index, beat.AudioData.Length);
+
+                index += step;
+            }
+
+            return new SampleSource(buffer, accentedBeat.WaveFormat);
+        }
+    }
+}
